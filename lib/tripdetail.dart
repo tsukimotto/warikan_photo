7d0bc095d6651cfe34e477bd96ee82c9c88a3a6f
Ethanlita <lita@ethanlita.com>
2022-07-25 19:05:41 +0900

    [Feature] Sidebar Usermail and User nickname display finished

--- lib/tripdetail.dart
+++ lib/tripdetail.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: use_key_in_widget_constructors
 
-import 'dart:js_util/js_util_wasm.dart';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
